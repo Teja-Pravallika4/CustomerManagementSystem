621277ae2e12de16d5dd4c946409fd384a38aaab
DELLPC1 <DELLPC1@DESKTOP-AA9KGR9>
2022-08-21 00:40:20 +0530

    Add project files.

---
+++ MVC/GlobalVariables.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Web;
+
+namespace MVC
+{
+    public class GlobalVariables
+    {
+        public static HttpClient WebAPIClient = new HttpClient();
+
+        static GlobalVariables()
+        {
+
+            WebAPIClient.BaseAddress = new Uri("https://localhost:44304/api/");
+            WebAPIClient.DefaultRequestHeaders.Clear();
+            WebAPIClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        }
+    }
+}
